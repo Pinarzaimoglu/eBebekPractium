3dc331669201977c448c7fbd795d45f44e70394e
PinarZaimoglu <zaimoglupinar@gmail.com>
2022-12-06 04:32:08 +0300

    Created project

---
+++ week2/flightTicketCalculator/src/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public int calculate() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mesafeyi km türünden giriniz :");
+        Integer distance = scanner.nextInt();
+    }
+}
