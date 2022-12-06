092197ba89b7b7bb50c4f4fe4551fa5cf5996119
PinarZaimoglu <zaimoglupinar@gmail.com>
2022-12-06 05:40:19 +0300

    Added Calculator

---
+++ week2/leapYear/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Yıl giriniz : ");
+        Integer year = scanner.nextInt();
+
+        if(year%100 == 0 && year%400 == 0)
+            System.out.println(year + " bir artık yıldır !");
+        else if(year%4 == 0)
+            System.out.println(year + " bir artık yıldır !");
+        else
+            System.out.println(year + " bir artık yıl değildir !");
+    }
+}
