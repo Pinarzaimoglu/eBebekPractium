1bb77bfbfd0f6f93dc6bb50c1d8fdfe5029390b9
PinarZaimoglu <zaimoglupinar@gmail.com>
2022-12-06 05:13:31 +0300

    Added calculator

--- week2/flightTicketCalculator/src/Main.java
+++ week2/flightTicketCalculator/src/Main.java
@@ -1,8 +1,38 @@
 import calculator.Calculator;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        Calculator calculator = new Calculator();
+
+        Integer distance, age, flightType;
+
+        do{
+            System.out.println("Mesafeyi km türünden giriniz :");
+            distance = scanner.nextInt();
+            if(distance<0)
+                System.out.println("Hatalı Veri Girdiniz !");
+        }while(distance<0);
+
+        do{
+            System.out.println("Yaşınızı giriniz :");
+            age = scanner.nextInt();
+            if(age<0)
+                System.out.println("Hatalı Veri Girdiniz !");
+        }while(age<0);
+
+        do{
+            System.out.println("Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ):");
+            flightType = scanner.nextInt();
+            if(flightType !=1 && flightType !=2)
+                System.out.println("Hatalı Veri Girdiniz !");
+        }while(flightType == 1 && flightType == 2);
+
+        calculator.calculate(distance, age, flightType);
+
     }
 }
